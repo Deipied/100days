ba56d734e46f644733c17d91a777fb273b64a056
Hoon Cho <hoonhyungcho@gmail.com>
2021-08-23 10:54:09 -0500

    completed blackjack capstone

--- day11_blackjack.py
+++ day11_blackjack.py
@@ -8,7 +8,99 @@ logo = """
       |  \/ K|                            _/ |                
       `------'                           |__/           
 """
-print(logo)
+import random
+end_of_game = True
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+random_card = random.choice(cards)
+your_cards = []
+computer_cards = []
+your_score = sum(your_cards)
+computer_score = sum(computer_cards)
+
+
+play_blackjack = input("Would you like to play a game of Blackjack? Type 'y' or 'n': ")
+
+if play_blackjack == 'y':
+      end_of_game = False
+
+while end_of_game == True:
+      play_blackjack = input("Would you like to play a game of Blackjack? Type 'y' or 'n': ")
+
+def receive_card(x):
+      x.append(random_card)
+
+def check_score():
+      global your_score
+      global computer_score
+      if your_score == 21 and computer_score < 10:
+            print("BlackJack! You win!")
+            end_of_game == True
+      elif your_score > 21 and 11 in your_score:
+            your_score -= 10
+      elif computer_score > 21 and 11 in computer_score:
+            computer_score -= 10
+      elif your_score > 21:
+            end_of_game == True
+            end_of_game_calc()      
+
+
+def current_standing():
+      global your_score
+      global computer_score
+      print(f"Your cards: {your_cards}, current score: {your_score}\nComputer's first card: {computer_cards}")
+
+def your_turn():
+      another_card = input("Type 'y' to get another card, type 'n' to pass: ")
+      if another_card == 'y':
+            receive_card(your_cards)
+            check_score()
+            your_turn()
+      elif another_card == 'n':
+            computer_turn()
+
+def computer_turn():
+      receive_card(computer_cards)
+      check_score()
+      if computer_score == 21:
+            end_of_game = True
+            end_of_game_calc()
+      elif computer_score >= 17:
+            end_of_game_calc()
+      else:
+            computer_turn()
+
+while end_of_game == False:
+      print(logo)
+      receive_card(your_cards)
+      receive_card(your_cards)
+      receive_card(computer_cards)
+      current_standing()
+      check_score()
+      your_turn()
+
+
+def end_of_game_calc():
+      print(f"Your final hand: {your_cards}, final score: {your_score}\nComputer's final hand: {computer_cards}, final score: {computer_score}")
+      if your_score > 21:
+            print("You lose!")
+      elif computer_score > 21:
+            print("You win!")
+      elif your_score > computer_score:
+            print("You win!")
+      elif computer_score > your_score:
+            print("You lose!")
+      elif computer_score == your_score:
+            print("Draw!")
+
+
+
+
+            
+      
+      
+
+
+
 
 ############### Blackjack Project #####################
 
@@ -42,19 +134,3 @@ print(logo)
 
 #Hint 3: Download and read this flow chart I've created: 
 #   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
-
-def two_sum(nums, target):
-    num1 = 0
-    num2 = 0
-    for num in nums:
-        num1 = nums.index(num)
-        print(num1)
-        for numbers in nums:
-            num2 = nums.index(numbers)
-            if nums[num1] + nums[num2] == target:
-                answer = [num1, num2]
-                return answer
-    
-    
-    
-print(two_sum([2,7,11,15], 9))
