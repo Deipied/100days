bcc15b671912c40b053a75754fc9a7af5e65eeb3
Hoon Cho <hoonhyungcho@gmail.com>
2021-09-02 09:51:27 -0500

    figuring out how STDIN works

--- day18/STDIN INPUT/STDIN.py
+++ day18/STDIN INPUT/STDIN.py
@@ -1,27 +1,27 @@
-import re
-import sys
+# import re
+# import sys
 
-def camelCaseFour(s):
-    if s[0] == "S":
-        if s[2] == "M":
-            s = s[:-2]
-        s = re.findall('.[^A-Z]*', s[4:])
-        for i in range(len(s)):
-            s[i] = s[i].lower()                        
-        return " ".join(s)
-    elif s[0] == "C":
-        str = s[4:].split()
-        if s[2] == "M":
-            for i in range(1, len(str)):
-                str[i] = str[i].capitalize()
-            str.append("()")
-        elif s[2] == "V":
-            for i in range(1, len(str)):
-                str[i] = str[i].capitalize()
-        elif s[2] == "C":
-            for i in range(0, len(str)):
-                str[i] = str[i].capitalize()
-        return "".join(str)
+# def camelCaseFour(s):
+#     if s[0] == "S":
+#         if s[2] == "M":
+#             s = s[:-2]
+#         s = re.findall('.[^A-Z]*', s[4:])
+#         for i in range(len(s)):
+#             s[i] = s[i].lower()                        
+#         return " ".join(s)
+#     elif s[0] == "C":
+#         str = s[4:].split()
+#         if s[2] == "M":
+#             for i in range(1, len(str)):
+#                 str[i] = str[i].capitalize()
+#             str.append("()")
+#         elif s[2] == "V":
+#             for i in range(1, len(str)):
+#                 str[i] = str[i].capitalize()
+#         elif s[2] == "C":
+#             for i in range(0, len(str)):
+#                 str[i] = str[i].capitalize()
+#         return "".join(str)
 
 
 # if __name__ == '__main__':
@@ -32,7 +32,25 @@ def camelCaseFour(s):
 #     for line in input_list:
 #         print(camelCaseFour(line))
 
-print(camelCaseFour("S;M;sweatTea()"))
-print(camelCaseFour("S;V;epsonPrinter"))
-print(camelCaseFour("C;M;santa claus"))
-print(camelCaseFour("C;C;mirror"))
+# print(camelCaseFour("S;M;sweatTea()"))
+# print(camelCaseFour("S;V;epsonPrinter"))
+# print(camelCaseFour("C;M;santa claus"))
+# print(camelCaseFour("C;C;mirror"))
+
+def divisibleSumPairs(n, k, ar):
+    pair_count = 0
+    array = []
+    for i in range(1, n):
+        array.append(i)
+
+    for j in range(n):
+        for l in array:
+            if (ar[j] + ar[l]) % k == 0:
+                pair_count += 1
+        if array:
+            array.pop(0)
+        
+            
+    return pair_count
+
+print(divisibleSumPairs(6, 3, [1, 3, 2, 6, 1, 2]))
