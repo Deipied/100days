78efeb338cebe3caa123918b67dfd783b7342f7a
Hoon Cho <hoonhyungcho@gmail.com>
2021-09-03 10:37:48 -0500

    finished day19 project

--- day19/main.py
+++ day19/main.py
@@ -1,37 +1,38 @@
 from turtle import Turtle, Screen
+import random
 
-tim = Turtle()
+is_race_on = False
 screen = Screen()
-tim.speed("fastest")
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
+all_turtles = []
 
-def move_forward():
-    tim.forward(20)
+#Create 6 turtles
+for turtle_index in range(0, 6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
 
-def move_backward():
-    tim.backward(20)
+if user_bet:
+    is_race_on = True
 
-def turn_left():
-    tim.left(10)
+while is_race_on:
+    for turtle in all_turtles:
+        #230 is 250 - half the width of the turtle.
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
 
-def turn_right():
-    tim.right(10)
+        #Make each turtle move a random amount.
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
-def clear():
-    tim.clear()
-    tim.penup()
-    tim.home()
-    tim.pendown()
-
-for i in range(3, 12):
-    n = 180 - (180(i-2))/3
-    for _ in range(12):
-        tim.forward(4 + 2 * i)
-        tim.left(n)
-    
-    
-
-
-
-screen.listen()
-screen.onkey(clear, "c")
 screen.exitonclick()
