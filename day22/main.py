2528cff43534d8378e142d1dc5fd8d44ad2dcb2a
Hoon Cho <hoonhyungcho@gmail.com>
2021-09-11 12:09:23 -0500

    intialized day 22

---
+++ day22/main.py
@@ -0,0 +1,30 @@
+from turtle import Turtle,Screen
+from paddle import Paddle
+from ball import Ball
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
+
+screen.listen()
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    ball.movement()
+    
+
+
+screen.exitonclick()
