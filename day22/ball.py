4982d7b8d9ae798cf80b520c8d4af704b2f3878e
Hoon Cho <hoonhyungcho@gmail.com>
2021-09-13 16:05:46 -0500

    reinstated previous commit

--- day22/ball.py
+++ day22/ball.py
@@ -14,19 +14,3 @@ class Ball(Turtle):
         new_x = self.xcor() + 10
         new_y = self.ycor() + 10
         self.goto(new_x, new_y)
-
-
-class Ball(Turtle):
-
-    def __init__(self):
-        super().__init__()
-        self.color("white")
-        self.shape("circle")
-        self.penup()
-        self.speed("fast")
-        self.setheading(45)
-
-    def movement(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
-        self.goto(new_x, new_y)
