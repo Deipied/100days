f4eb67009e30c8db9ea14b1f2c5f31e96abcbf55
Hoon Cho <hoonhyungcho@gmail.com>
2021-09-28 09:32:52 -0500

    re-organized and revisited snkae game

--- day11-20/day20/main.py
+++ day11-20/day20/main.py
@@ -1,7 +1,7 @@
-from food import Food
 from turtle import Screen
 from snake import Snake
-from score import Score
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -12,7 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
-score = Score()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -26,19 +26,26 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    #Detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
-        score.add_score()
         snake.extend()
+        scoreboard.increase_score()
 
+    #Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
-        score.game_over()
+        scoreboard.game_over()
 
+    #Detect collision with tail.
     for segment in snake.segments:
-        if snake.head.distance(segment[1:]) < 10:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
             game_is_on = False
-            score.game_over()
+            scoreboard.game_over()
+
+
 
 
 
--- day11-20/day20/snake.py
+++ day11-20/day20/snake.py
@@ -1,6 +1,10 @@
 from turtle import Turtle
-STARTING_POSITIONS = [(0,0), (-20,0), (-40,0)]
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
@@ -14,15 +18,15 @@ class Snake:
         for position in STARTING_POSITIONS:
             self.add_segment(position)
 
-    def extend(self):
-        self.add_segment(self.segments[-1].position())
-
-    def add_segment(self):
+    def add_segment(self, position):
         new_segment = Turtle("square")
         new_segment.color("white")
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -32,13 +36,17 @@ class Snake:
         self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        self.head.setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        self.head.setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        self.head.setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
     def right(self):
-        self.head.setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
