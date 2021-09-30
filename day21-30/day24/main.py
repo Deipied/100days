f080660026441441b18637a174b098c43fbee611
Hoon Cho <hoonhyungcho@gmail.com>
2021-09-30 13:45:39 -0500

    finished day 26 and finalized at work

--- day21-30/day24/main.py
+++ day21-30/day24/main.py
@@ -2,5 +2,6 @@
 #     contents = file.read()
 #     print(contents)
 
-with open("new_file.txt", mode="w") as file:
-    file.write("\nNew text.")
+# with open("new_file.txt", mode="w") as file:
+#     file.write("\nNew text.")
+
