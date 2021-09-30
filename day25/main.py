fc76577160aa703f9ac96bd24f5f90fb19a16038
Hoon Cho <hoonhyungcho@gmail.com>
2021-09-30 09:49:50 -0500

    finished day 25

--- day25/main.py
+++ day25/main.py
@@ -1,15 +1,30 @@
-# import csv
-
-# with open("weather_Data.csv") as data_file:
-#     data = csv.reader(data_file)
-#     temperatures = []
-#     for row in data:
-#         if row[1] != "temp":    
-#             temperatures.append(int(row[1]))
-#     print(temperatures)
-
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+# data = pandas.read_csv("weather_data.csv")
 
+# monday = data[data.day == "Monday"]
+# monday_temp = int(monday.temp)
+
+
+# print((monday_temp*(9/5)) + 32)
+
+data = pandas.read_csv("Squirrel_Data.csv")
+primary_color = data["Primary Fur Color"]
+red = 0
+grey = 0
+black = 0
+for color in primary_color:
+    if color == "Cinnamon":
+        red += 1
+    elif color == "Gray":
+        grey += 1
+    elif color == "Black":
+        black += 1
+
+color_data ={
+    "Color": ["grey", "red", "black"],
+    "Count" : [red, grey, black]
+}
+
+new_data = pandas.DataFrame(color_data)
+new_data.to_csv("squirrel_count.csv")
